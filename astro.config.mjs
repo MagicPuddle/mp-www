6c91c208196289980ad6febef9d4e6b8f18b3fc6
mcinotti <martincinotti@gmail.com>
2023-07-01 19:14:22 -0400

    section corners

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import netlify from "@astrojs/netlify/functions";
+// import netlify from '@astrojs/netlify/edge-functions'; no compat with builders below
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://magicpuddle.netlify.app',
-	integrations: [mdx(), sitemap()],
+  site: 'https://magicpuddle.netlify.app',
+  integrations: [mdx(), sitemap(), react()],
+  output: "server",
+  adapter: netlify({
+    builders: true
+  })
 });
