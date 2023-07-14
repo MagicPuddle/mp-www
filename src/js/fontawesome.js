6d6e67e4c2cc2bf494a0bfc623e22fa08eae1037
mcinotti <martincinotti@gmail.com>
2023-07-13 23:34:06 -0400

    migrating old layout to new BaseLayout component

--- src/js/fontawesome.js
+++ src/js/fontawesome.js
@@ -1,4 +1,4 @@
-// import { library } from "@fontawesome/fontawesome-svg-core";
-// import { fas } from "@fontawesome/free-solid-svg-icons";
+import { library } from "@fontawesome/fontawesome-svg-core";
+import { fas } from "@fontawesome/free-solid-svg-icons";
 
-// library.add(fas);
+library.add(fas);
