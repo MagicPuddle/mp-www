e980e267256d5c7c80ba285e6150b524849ad118
mcinotti <martincinotti@gmail.com>
2023-09-08 12:43:57 -0400

    Revert to pre node fail comit fixed configs pkgs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-// import netlify from "@astrojs/netlify/functions";
+import robotsTxt from "astro-robots-txt";
+import netlify from "@astrojs/netlify/functions";
 // import netlify from '@astrojs/netlify/edge-functions'; no compat with builders below
-import react from "@astrojs/react";
-import svelte from "@astrojs/svelte";
-import { netlifyFunctions } from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://magicpuddle.netlify.app',
-  integrations: [mdx(), sitemap(),netlify(), nodemailer()],
+  integrations: [mdx(), sitemap(), robotsTxt()],
   output: 'server',
   publish: 'dist',
   adapter: netlify({
--- src/content/docs/docs.md
+++ src/content/docs/docs.md
@@ -1,9 +1,9 @@
 ---
-title: Docs
+title: Documentation
 description: MagicPuddle Documentation
-pubDate: 'Jul 02 2022'
-heroImage: '/placeholder-mp.png'
-slug: docs
+pubDate: Jul 02 2022
+heroImage: /placeholder-mp.png
+slug: documentation
 ---
 
 # Documentation
